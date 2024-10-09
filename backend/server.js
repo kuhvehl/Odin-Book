35b1aaed7ff821fde058e418d04652d3dc2b2985
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-10-09 17:37:39 -0400

    Implement register functionality

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const userRoutes = require("./userRoutes");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, Odin-Book!");
---
+++ backend/userRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, name } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    res
+      .status(201)
+      .json({ message: "User created successfully", userId: user.id });
+  } catch (error) {
+    res.status(500).json({ error: "Error creating user" });
+  }
+});
+
+module.exports = router;
